6657856ad735ec26f36ca2792ad48ad923007c2e
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-05-21 14:39:08 +0300

    adding icons

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,15 +1,23 @@
 import React from "react";
+import { FaBeer } from 'react-icons/fa';
+
 
 const SideBar = () => {
   return (
     <div className="fixed top-0 h-screen w-16 flex flex-col
      bg-primary text-secondary shadow">
-      <i>a</i>
-      <i>a</i>
-      <i>a</i>
-      <i>a</i>
+      <SideBarIcon icon={<FaBeer size={26}/>}/>
+      <SideBarIcon icon={<FaBeer size={26}/>}/>
+      <SideBarIcon icon={<FaBeer size={26}/>}/>
+      <SideBarIcon icon={<FaBeer size={26}/>}/>
     </div>
   );
 };
+
+const SideBarIcon =({ icon }) => (
+    <div className="sidebar-icon">
+        {icon}
+    </div>
+)
 
 export default SideBar;
