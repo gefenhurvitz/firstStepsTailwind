97ae2bb16c088b3bf2dfcc1a5bfa0653de4ae67b
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-05-21 13:33:43 +0300

    now it works well

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-
-  ],
-};
+  plugins: [],
+}
